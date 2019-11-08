b8d0cebde5b009c434ab388bcbb11b9950ff2f9c
ebrugulec <ebru@ebrugulec.com>
2019-11-08 13:27:50 +0300

    scrum master voting

--- src/components/ScrumMaster/ScrumMaster.js
+++ src/components/ScrumMaster/ScrumMaster.js
@@ -3,6 +3,7 @@ import * as firebase from 'firebase'
 
 const STORY_POINTS = [1,2,3,5,8,21,34,55,89,134]
 
+//TODO: Add Initial Values
 class ScrumMaster extends React.Component {
     constructor() {
         super();
@@ -15,7 +16,9 @@ class ScrumMaster extends React.Component {
             infoText: 'Please Vote !!!',
             intervalId: 0,
             isShowVote: false,
-            votes: []
+            votes: [],
+            isStopVoting: false,
+            finalScore: 1
         }
     }
 
@@ -23,7 +26,11 @@ class ScrumMaster extends React.Component {
         console.log(event)
     }
 
-    async componentDidMount () {
+    componentDidMount () {
+        this.callFunction()
+    }
+
+    callFunction = async () => {
         const snapshot = await firebase.database().ref('scrum/').once('value', (snapshot) => {
             return snapshot
         })
@@ -32,7 +39,6 @@ class ScrumMaster extends React.Component {
         let activeStory = scrum.storyList.filter((story) => {
             return story.status !== 'Not Voted'
         })
-        console.log('storyList', activeStory[0])
 
         this.setState({
             storyList: scrum.storyList,
@@ -49,7 +55,6 @@ class ScrumMaster extends React.Component {
      }
 
     handleVote = (value) => {
-        console.log(value)
         firebase.database().ref('vote/').set({
             sc_master: value
         }).then((data) => {
@@ -65,19 +70,10 @@ class ScrumMaster extends React.Component {
     }
 
     getVotes = async () => {
-        const snapshot = await firebase.database().ref('vote/').once('value', (snapshot) => {
-            return snapshot
-        })
-
-        console.log('votes', snapshot.val())
-    }
-
-    getVotes = async () => {
         const { votersCount } = this.state
         const snapshot = await firebase.database().ref('vote/').once('value', (snapshot) => {
             return snapshot
         })
-        console.log('snapshot.val()', snapshot.val())
 
         if (snapshot.val() !== null) {
             const vote_count = Object.keys(snapshot.val()).length
@@ -85,9 +81,6 @@ class ScrumMaster extends React.Component {
             //TODO: type check (===)
             if (vote_count == votersCount) {
                 clearInterval(this.state.intervalId)
-                this.setState({
-                    isShowVote: true,
-                })
             }
             this.setState({
                 votes: snapshot.val()
@@ -97,24 +90,95 @@ class ScrumMaster extends React.Component {
         }
     }
 
-    endVoting = () => {
-        this.setState({
-            isShowVote: true
+    endVoting = async () => {
+        const { votersCount } = this.state
+        const snapshot = await firebase.database().ref('vote/').once('value', (snapshot) => {
+            return snapshot
         })
-        clearInterval(this.state.intervalId)
+
+        const vote_count = Object.keys(snapshot.val()).length
+        if ((votersCount == vote_count) && vote_count !== undefined) {
+            this.setState({
+                isShowVote: true,
+                isStopVoting: true
+            })
+            clearInterval(this.state.intervalId)
+        } else {
+            alert('Please wait')
+        }
+        
+    }
+
+    renderVoters = () => {
+        const { votersCount, votes, isShowVote } = this.state
+        const voters = [];
+
+        if (isShowVote && votes !== []) {
+            for (let i = 1; i < votersCount; i++) {
+                voters.push(<div><span>Voter {i}: {votes[i]}</span></div>);
+            }
+            voters.push(<div><span>ScrumMaster: {votes.sc_master}</span></div>)
+            
+        } else {
+            for (let i = 1; i < votersCount; i++) {
+                voters.push(<div><span>Voter {i}: {votes[i] !== undefined ? 'Voted' : 'Not Voted'}</span></div>)
+            }
+            voters.push(<div><span>ScrumMaster: {votes.sc_master !== undefined ? 'Voted': 'Not Voted'}</span></div>)
+        }
+
+        return voters
+    }
+
+    handleFinalScore = (value) => {
+        this.setState({
+            finalScore: value
+        })
+    }
+
+    saveFinalScore = () => {
+        //TODO: Remove vote table from firebase
+        const { storyList, finalScore } = this.state
+        let newStoryList = storyList
+        for (let i in storyList) {
+            if (newStoryList[i].status === 'Active') {
+                newStoryList[i].status = 'Voted'
+                newStoryList[i].story_point = finalScore
+                var nextActiveIndex = Number(i)+1
+            }
+        }
+        if (newStoryList[nextActiveIndex] !== undefined) {
+            newStoryList[nextActiveIndex].status = 'Active'
+        } 
+
+        firebase.database().ref('scrum/').update({
+            storyList: newStoryList,
+        }).then((data)=>{
+            this.setState({
+                isShowVote: false,
+                storyList: newStoryList,
+                votersCount: 0,
+                votes: [],
+                isStopVoting: false
+            })
+        }).catch((error)=>{
+            //error callback
+        })
+        //TODO: Burayi degistir
+
+        firebase.database().ref('vote/').set({
+        }).then((data) => {
+            
+        }).catch((error)=>{
+        })
+        this.callFunction()
     }
     render() {
-        const { sessionName, numberVoters, storyList, activeStory, votersCount, infoText, votes, isShowVote } = this.state
-        const voters = [];
-        for (let i = 1; i < votersCount; i++) {
-            console.log('hey', votes[1])
-            voters.push(<div><span>Voter {i}: {votes[i] !== undefined ? 'Voted' : 'Not Voted'}</span></div>);
-        }
+        const { sessionName, numberVoters, storyList, activeStory, votersCount, infoText, votes, isShowVote, isStopVoting } = this.state
         return (
             <div>
                 {storyList.map((story) => {
                     return <div>
-                        <div>{story.story_name} - {story.story_point === 0 ? '' : story.story_point} - {story.status}</div>
+                        <div>{story.story_name} - {story.story_point == 0 ? '' : story.story_point} - {story.status}</div>
                     </div>
                 })}
 ----------
@@ -128,10 +192,17 @@ class ScrumMaster extends React.Component {
                     -------------
                 <div>
                     Scrum Master Panel
-                    {voters}
-                    ScrumMaster: {(votes.sc_master !== undefined && !isShowVote) ? votes.sc_master : 'Not Voted'}
+                    {this.renderVoters()}
                 </div>
-                <button onClick={this.endVoting}>End Voting</button>
+                {
+                    isStopVoting ?
+                    <div>
+                        <input onChange={(e) => this.handleFinalScore(e.target.value)}></input>
+                        <button onClick={this.saveFinalScore}>Save Final Score</button>
+                    </div>
+                    :
+                    <button onClick={this.endVoting}>End Voting</button>
+                }
             </div>
         )
     }
