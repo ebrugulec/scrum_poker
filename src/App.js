e7b3e5716f292c0f6e0d0dfcb93095059d3f0943
ebrugulec <ebru@ebrugulec.com>
2019-11-09 05:16:09 +0300

    remove comment line from app.js

--- src/App.js
+++ src/App.js
@@ -1,58 +1,30 @@
 import React from 'react';
-import './App.scss';
-
-import StoryList from '../src/components/StoryList/StoryList'
-import Developer from '../src/components/Developer/Developer'
-import ScrumMaster from '../src/components/ScrumMaster/ScrumMaster'
-
 import {
   BrowserRouter,
   Switch,
   Route,
   Link
 } from "react-router-dom";
+import StoryList from '../src/components/StoryList/StoryList'
+import Developer from '../src/components/Developer/Developer'
+import ScrumMaster from '../src/components/ScrumMaster/ScrumMaster'
+
 import * as ROUTES from '../src/routes/routes'
+import './App.scss';
 
 class App extends React.Component {
-  componentDidMount () {
-  //   firebase.database().ref('scrum/').once('value', function (snapshot) {
-  //     console.log(snapshot.val())
-  // });
-
-  // firebase.database().ref('scrum/').update({
-  //   deneme: ['huloo', 'and'],
-  // }).then((data)=>{
-  //       //success callback
-  //       console.log('data ' , data)
-  //   }).catch((error)=>{
-  //       //error callback
-  //       console.log('error ' , error)
-  //   })
-
-  // firebase.database().ref('scrum/').set({
-  //     deneme: ['csfjdgd', 'a', 'b'],
-  // }).then((data)=>{
-  //     //success callback
-  //     console.log('data ' , data)
-  // }).catch((error)=>{
-  //     //error callback
-  //     console.log('error ' , error)
-  // })
-  }
   render () {
-
-  return (
-    <div className='main-wrapper'>
-      <BrowserRouter>
-        <Switch>
-          <Route exact path={ROUTES.POKER_PLANNING} component={StoryList} />
-          <Route exact path={ROUTES.SCRUM_MASTER} component={ScrumMaster} />
-          <Route exact path={ROUTES.DEVELOPER} component={Developer} />
-        </Switch>
-      </BrowserRouter>
-
-    </div>
-  );
+    return (
+      <div className='main-wrapper'>
+        <BrowserRouter>
+          <Switch>
+            <Route exact path={ROUTES.POKER_PLANNING} component={StoryList} />
+            <Route exact path={ROUTES.SCRUM_MASTER} component={ScrumMaster} />
+            <Route exact path={ROUTES.DEVELOPER} component={Developer} />
+          </Switch>
+        </BrowserRouter>
+      </div>
+    );
   }
 }
 
---
+++ src/components/View/DeveloperLink.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const DeveloperLink = (props) => {
+    return (
+        <Button
+            className={className}
+            onClick={onClick}
+            type={type}>
+            {text}
+        </Button>
+    )
+}
+
+export default DeveloperLink
